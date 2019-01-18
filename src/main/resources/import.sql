f7f1a756267adb22f22ae34019425d7d369063ba
taocon <conradtta@gmail.com>
2019-01-18 13:58:56 +0000

    back

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO CLASSROOM (classroomId,trainerName) VALUES (1, 'John Gordon');
 INSERT INTO CLASSROOM (classroomId,trainerName) VALUES (2, 'Matt Hunt');
-INSERT INTO TRAINEE (traineeId,trainerFullName) VALUES (1,'Joe Bloggs',1 );
-INSERT INTO TRAINEE (traineeId,trainerFullName) VALUES (2,'Jane Bloggs',1);
-INSERT INTO TRAINEE (traineeId,trainerFullName) VALUES (3,'David Bloggs',2);
-INSERT INTO TRAINEE (traineeId,trainerFullName) VALUES (4,'Ceri Bloggs',2);
+INSERT INTO TRAINEE (traineeId,trainerFullName) VALUES (1,'Joe Bloggs');
+INSERT INTO TRAINEE (traineeId,trainerFullName) VALUES (2,'Jane Bloggs');
+INSERT INTO TRAINEE (traineeId,trainerFullName) VALUES (3,'David Bloggs');
+INSERT INTO TRAINEE (traineeId,trainerFullName) VALUES (4,'Ceri Bloggs');
