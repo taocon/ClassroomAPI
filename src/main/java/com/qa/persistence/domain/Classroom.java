38a4f7820dd382445e0214b683149a36dc872c2a
taocon <conradtta@gmail.com>
2019-01-18 12:50:24 +0000

    break

--- src/main/java/com/qa/persistence/domain/Classroom.java
+++ src/main/java/com/qa/persistence/domain/Classroom.java
@@ -1,9 +1,13 @@
 package com.qa.persistence.domain;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -15,13 +19,17 @@ public class Classroom {
 
 	private String trainerName;
 
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "classroom")
+	private List<Trainee> trainee;
+
 	public Classroom() {
 
 	}
 
-	public Classroom(String trainerName) {
+	public Classroom(String trainerName, List<Trainee> trainee) {
 
 		this.setTrainerName(trainerName);
+		this.setTrainee(trainee);
 	}
 
 	public String getTrainerName() {
@@ -31,13 +39,21 @@ public class Classroom {
 	public void setTrainerName(String trainerName) {
 		this.trainerName = trainerName;
 	}
-	
+
 	public Long getClassroomId() {
 		return classroomId;
 	}
 
 	public void setClassroomId(Long classroomId) {
 		this.classroomId = classroomId;
+	}
+
+	public List<Trainee> getTrainee() {
+		return trainee;
+	}
+
+	public void setTrainee(List<Trainee> trainee) {
+		this.trainee = trainee;
 	}
 
 }
